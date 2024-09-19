2c4736c72110d87f4efcc624df6d8a82b151fa8f
Dafne Duda <dafne.leslie@gmail.com>
2024-09-19 19:59:51 -0300

    exercises ArrayList

--- ClassMaterials/ArraysAndLists/src/main/java/com/rhit/arraysandlists/ArrayListFunctions.java
+++ ClassMaterials/ArraysAndLists/src/main/java/com/rhit/arraysandlists/ArrayListFunctions.java
@@ -1,7 +1,7 @@
 package com.rhit.arraysandlists;
 
 import java.util.ArrayList;
-
+import java.util.Collections;
 
 public class ArrayListFunctions {
 
@@ -12,8 +12,17 @@ public class ArrayListFunctions {
      * So arrayListOfAs({"Abc","foo","AAA", "bar"}) yields ["Abc","AAA"]
      */
     public static ArrayList<String> arraylistOfAs(String[] strings) {
-        // TODO: Solve me
-        return new ArrayList<String>();
+        // criar um array de palavras
+        // percorrer o array e ver as palavras que começam com A
+        // colocar numa Arraylist
+
+        ArrayList<String> palavrasComA = new ArrayList<String>();
+        for (String palavra : strings) {
+            if (palavra.startsWith("A")) {
+                palavrasComA.add(palavra);
+            }
+        }
+        return palavrasComA;
     }
 
     /**
@@ -23,7 +32,9 @@ public class ArrayListFunctions {
      * so insertIntoSorted([1,5,9],6) yields [1,5,6,9]
      */
     public static void insertIntoSorted(ArrayList<Integer> list, int number) {
-        // TODO: Solve me
+
+        list.add(number);
+        Collections.sort(list);
     }
 
     /**
@@ -32,6 +43,9 @@ public class ArrayListFunctions {
      * So removeLongStrings(["a","xxxx","b","zzzzz"]) yields ["a","b"]
      */
     public static void removeLongStrings(ArrayList<String> strings) {
-        // TODO: Solve me
+
+        for (int indice = strings.size()-1; indice >=0; indice--){
+            if (strings.get(indice).length() >= 3) strings.remove(indice);
+        }
     }
 }
